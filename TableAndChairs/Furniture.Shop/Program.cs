8f2857ab4695e9ead8ee3d090af8534a7bf47aa1
Thomas Adams <tjackadams@itadams.co.uk>
2020-08-26 08:36:27 +0100

    scaffold stored procedures

--- TableAndChairs/Furniture.Shop/Program.cs
+++ TableAndChairs/Furniture.Shop/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Data;
+using System.Threading.Tasks;
+using Furniture.Shop.Data;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Furniture.Shop
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var services = new ServiceCollection()
+                .AddDbContext<FurnitureContext>()
+                .AddTransient(sp =>
+                    new FurnitureContextProcedures(sp.GetRequiredService<FurnitureContext>()));
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var proc = serviceProvider.GetRequiredService<FurnitureContextProcedures>();
+
+            var parameter = new DataTable();
+            parameter.Columns.Add("Value", typeof(int));
+            parameter.Rows.Add(1);
+
+            foreach (var shipment in await proc.GetShipments(parameter))
+            {
+                Console.WriteLine("Shipment: {0}", System.Text.Json.JsonSerializer.Serialize(shipment));
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
